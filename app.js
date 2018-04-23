354ed1945ae6cdbe75589eff7f764c10b968ff9d
Seth Silesky <seth.silesky@gmail.com>
2018-04-23 13:36:09 -0500

    pass header path

--- app.js
+++ app.js
@@ -13,15 +13,18 @@ const fs = require('fs');
 const args = require('minimist')(process.argv.slice(2));
 const isGlob = require('is-glob');
 
-const year = new Date().getFullYear()
-const header = `/*******************************************************************************
-  * Licensed Materials - Property of IBM
-  * (c) Copyright IBM Corporation ${year}. All Rights Reserved.
-  *
-  * Note to U.S. Government Users Restricted Rights:
-  * Use, duplication or disclosure restricted by GSA ADP Schedule
-  * Contract with IBM Corp.
-  *******************************************************************************/\n\n`;
+const flatten = arr => [].concat(...arr);
+const [headerPath, ...filesOrGlobs] = flatten(Object.values(args));
+
+let headerTxt = '';
+try {
+  headerTxt = require(headerPath);
+} catch (err) {
+  console.log(
+    `Unable to prepend, because no valid header.js found. Header argument passed: ${headerPath}`,
+  );
+  return null;
+}
 
 const readFilePromise = filePath =>
   new Promise((resolve, reject) => {
@@ -31,20 +34,24 @@ const readFilePromise = filePath =>
     });
   });
 
-const prependLicense = filePath =>
+const prependHeader = filePath =>
   new Promise((resolve, reject) => {
-    prependFile(filePath, header, err => {
-      if (err) reject(err);
+    // grab headerTxt from global
+    prependFile(filePath, headerTxt, err => {
+      if (err) {
+        console.log('Could not prepend header.');
+        reject(err);
+      }
       console.log(`Prepended license to ${filePath}`);
       resolve();
     });
   });
 
-const conditionallyReadAndPrependFile = path => {
-  readFilePromise(path).then(output => {
+const conditionallyReadAndPrependHeaderToFile = filePath => {
+  readFilePromise(filePath).then(output => {
     // don't accidentally double-append.
-    if (!output.includes(header)) {
-      prependLicense(path);
+    if (!output.includes(headerTxt)) {
+      prependHeader(filePath);
     }
   });
 };
@@ -52,6 +59,11 @@ const conditionallyReadAndPrependFile = path => {
 const isFile = path => {
   return fs.existsSync(path) ? !fs.statSync(path).isDirectory() : false;
 };
+
+// all this complex logic to basically check if we user is passing a glob (e.g. [src-web/**/*.js])
+// or an array of files
+// (natural bash globbing e.g  [ 'src-web/thing/thing2/after.scss', 'src-web/thing/thing2/after1.scss'
+// so essentially, whether or not the thing has quotes should not matter.
 
 const getFilesFromGlob = globPattern => {
   try {
@@ -62,31 +74,24 @@ const getFilesFromGlob = globPattern => {
   }
 };
 
-// all this complex logic to basically check if we user is passing a glob (e.g. [src-web/**/*.js])
-// or an array of files
-// (natural bash globbing e.g  [ 'src-web/thing/thing2/after.scss', 'src-web/thing/thing2/after1.scss'
-// so essentially, whether or not the thing has quotes should not matter.
-const flatten = arr => [].concat(...arr);
-
-const filesOrGlobs = flatten(Object.values(args));
 const fileList = filesOrGlobs.filter(isFile);
 const globList = filesOrGlobs.filter(isGlob);
 
 if (!filesOrGlobs.length) {
   console.log(
-    '--> Missing argument. Please include a valid directory path such as src-web or .',
+    '--> Missing argument. Please include a valid file or directory path such as src-web or .',
   );
 } else {
   // map over all files and prepend the license.
   if (globList.length) {
     const files = flatten(globList.map(getFilesFromGlob));
     if (files.length) {
-      files.forEach(conditionallyReadAndPrependFile);
+      files.forEach(conditionallyReadAndPrependHeaderToFile);
     } else {
       console.log('No files from glob found.');
     }
   }
   if (fileList.length) {
-    fileList.forEach(conditionallyReadAndPrependFile);
+    fileList.forEach(conditionallyReadAndPrependHeaderToFile);
   }
 }
